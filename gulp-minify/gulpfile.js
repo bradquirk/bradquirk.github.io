6c87d8df41e35264ae45f978ac2aeab87db7a7b1
Brad Quirk <brad@quirkconsulting.com.au>
2022-05-15 17:28:38 +1000

    added jira blog

--- gulp-minify/gulpfile.js
+++ gulp-minify/gulpfile.js
@@ -3,9 +3,11 @@ const {
     dest,
     watch
 } = require('gulp');
+const cache = require('gulp-cached')
 
 function copyAllFiles() {
     return src(['../../bradquirk.github.io_master/_site/**/*'])
+        .pipe(cache('copy'))
         .pipe(dest('../'));
 }
 
